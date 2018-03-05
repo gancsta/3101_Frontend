1fcac473d3fb8eb8862870e37215e64cae8629b0
Denys Stasyuk <gancsta@ukr.net>
2018-03-05 20:47:02 +0200

    Home work 26_02_2018

---
+++ 26_02_2018/custom.js
@@ -0,0 +1,27 @@
+// Задание 1
+// Сообщение приветствия
+alert("Приветствую");
+
+//Задание 2
+var firstName = 'Alex';
+var lastName = '';
+lastName = firstName;
+document.write(lastName);
+
+//Задание 3
+var yourName = 'Alex';
+var yourSurname = 'Lalala'
+var yourAge = 30;
+var yourEducation = 'Высшее';
+var yourHobbies = 'Футбол';
+
+//Задание 4
+document.write('<br>Задание 4: Будет ошибка так, как переменная \'x\' не определена')
+
+//Задание 5
+var a = prompt("Число 1: ");
+var b = prompt("Число 2: ");
+var res = parseInt(a) + parseInt(b);
+
+alert('Сума двох чисел равна: ' + res)
+
